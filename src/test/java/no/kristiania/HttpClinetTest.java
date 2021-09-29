fd476990c26df510f9fc67a383336e100ed8dbd4
yasersh <yaser.sharafaldin1234@gmail.com>
2021-09-29 14:27:56 +0200

    add initial test add initial test

---
+++ src/test/java/no/kristiania/HttpClinetTest.java
@@ -0,0 +1,13 @@
+package no.kristiania;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpClinetTest {
+    @Test
+    void shouldShowNumbrs(){
+        assertEquals(200,100+100);
+    }
+
+}
