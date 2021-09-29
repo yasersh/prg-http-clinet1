06cccabcc2c708337953ea993ac61c79ff892fc8
yasersh <yaser.sharafaldin1234@gmail.com>
2021-09-29 13:57:06 +0200

    make http conection to the server

--- src/main/java/no/kristiania/HttpClinet.java
+++ src/main/java/no/kristiania/HttpClinet.java
@@ -1,7 +1,21 @@
 package no.kristiania;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
 public class HttpClinet {
-    public static void main(String[] args) {
-        System.out.println("hallo world");
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("httpbin.org", 80);
+        socket.getOutputStream().write(
+                ("GET /html HTTP/1.1\r\n" +
+                "Host: httpbin.org\r\n"+
+                "\r\n") .getBytes());
+        InputStream in = socket.getInputStream();
+        int c ;
+        while ((c = in.read()) != -1){
+            System.out.print((char) c);
+
+        }
     }
 }
